f8e1e508582205aa3172c574f94b8ef099c8a854
Jordy Wijman <jordywijman@gmail.com>
2017-03-30 18:50:23 +0200

    Added notifications and saving in profiles in storage

---
+++ app/src/main/java/hva/groepje12/quitsmokinghabits/Profile.java
@@ -0,0 +1,53 @@
+package hva.groepje12.quitsmokinghabits;
+
+import java.util.Calendar;
+
+public class Profile {
+    private String firstName;
+    private String lastName;
+    private Calendar birthDate;
+    private Gender gender;
+
+    public enum Gender {male, female};
+
+    public Profile(String firstName, String lastName, Calendar birthDate, Gender gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.gender = gender;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getFullName() { return this.getFirstName() + " " + this.getLastName(); }
+
+    public void setBirthDate(Calendar birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Calendar getBirthDate() {
+        return this.birthDate;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Gender getGender() {
+        return this.gender;
+    }
+}
---
+++ app/src/main/java/hva/groepje12/quitsmokinghabits/RegisterActivity.java
@@ -0,0 +1,119 @@
+package hva.groepje12.quitsmokinghabits;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import java.sql.Time;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class RegisterActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+
+    EditText birthDate, firstName, lastName;
+    Button mEmailSignInButton, sendNotificationButton, sendTimedNotificationButton;
+
+    Calendar pickedTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        birthDate = (EditText) findViewById(R.id.edit_text_birth_date);
+        firstName = (EditText) findViewById(R.id.edit_text_first_name);
+        lastName = (EditText) findViewById(R.id.edit_text_last_name);
+
+        mEmailSignInButton = (Button) findViewById(R.id.button_create_account);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+
+        ProfileManager profileManager = new ProfileManager(this);
+        Profile profile = profileManager.getProfile();
+
+        Toast.makeText(this, "Hoi ik ben " + profile.getFullName(), Toast.LENGTH_LONG).show();
+
+        final Notification notify = new Notification("Klik hier om afgeleid te worden", getApplicationContext());
+        final Alarm alarm = new Alarm(00, 00, getApplicationContext());
+
+        final Button button_notify = (Button) findViewById(R.id.button_send_notification);
+        button_notify.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                notify.startNotification();
+            }
+        });
+
+        final Button button_alarm = (Button) findViewById(R.id.button_send_timed_notification);
+        button_alarm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                int hour = new Time(System.currentTimeMillis()).getHours();
+                int minutes = new Time(System.currentTimeMillis()).getMinutes();
+
+                alarm.setHour(hour);
+                alarm.setMinutes(minutes + 1);
+                alarm.startAlarm();
+            }
+        });
+    }
+
+    public void datePicker(View view) {
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.show(getFragmentManager(), "date");
+    }
+
+    private void setDate(final Calendar calendar) {
+        pickedTime = calendar;
+        final DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+        birthDate.setText(dateFormat.format(calendar.getTime()));
+    }
+
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        Calendar cal = new GregorianCalendar(year, month, day);
+        setDate(cal);
+    }
+
+    private void registerUser() {
+        Profile profile = new Profile(firstName.getText().toString(), lastName.getText().toString(), pickedTime, Profile.Gender.female);
+
+        Toast.makeText(this, "Profiel opgeslagen!", Toast.LENGTH_SHORT).show();
+
+        //Save the profile with a profile manager into the storage
+        ProfileManager profileManager = new ProfileManager(this);
+        profileManager.save(profile);
+    }
+
+    public static class DatePickerFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            return new DatePickerDialog(getActivity(),
+                    (DatePickerDialog.OnDateSetListener) getActivity(),year,month,day);
+        }
+    }
+
+}
+
