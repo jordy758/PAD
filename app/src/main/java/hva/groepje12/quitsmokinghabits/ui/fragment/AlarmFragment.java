9828c7d82b7ae8ea16cb3cd8fe7c21f1e3861040
DESKTOP-ROPT2AK\Sebas <sebastiaan.hulst@hva.nl>
2017-05-02 14:47:19 +0200

    OnClick Time

--- app/src/main/java/hva/groepje12/quitsmokinghabits/ui/fragment/AlarmFragment.java
+++ app/src/main/java/hva/groepje12/quitsmokinghabits/ui/fragment/AlarmFragment.java
@@ -1,6 +1,8 @@
 package hva.groepje12.quitsmokinghabits.ui.fragment;
 
 import android.annotation.TargetApi;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -10,12 +12,14 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.app.Activity;
 import java.util.Calendar;
+import android.app.TimePickerDialog;
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
 
 import hva.groepje12.quitsmokinghabits.R;
 
-public class AlarmFragment extends Fragment  {
+public class AlarmFragment extends Fragment {
 
     public TextView time;
     TimePicker TimePicker;
@@ -23,7 +27,6 @@ public class AlarmFragment extends Fragment  {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     private void set_text(String output) {
@@ -34,17 +37,17 @@ public class AlarmFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //return inflater.inflate(R.layout.alarms_fragment_main, container, false);
+
         View rootView = inflater.inflate(R.layout.alarms_fragment_main, container, false);
 
         time = (TextView) rootView.findViewById(R.id.time);
-        TimePicker = (TimePicker) rootView.findViewById(R.id.TimePicker);
-        TimePicker.setIs24HourView(true);
+        //TimePicker = (TimePicker) rootView.findViewById(R.id.TimePicker);
+        //TimePicker.setIs24HourView(true);
         final Calendar calendar = Calendar.getInstance();
 
         Button add_time = (Button) rootView.findViewById(R.id.add_time);
 
-        // create an onClick listener to start the alarm
+        /* create an onClick listener to start the alarm
         add_time.setOnClickListener(new View.OnClickListener() {
             @TargetApi(Build.VERSION_CODES.M)
             @Override
@@ -68,8 +71,32 @@ public class AlarmFragment extends Fragment  {
 
             }
 
-        });
+        }); */
         return rootView;
+    }
+
+
+
+    public void timePicker(View view) {
+        TimePickerFragment newFragment = new TimePickerFragment();
+        newFragment.show(getFragmentManager(), "time");
+    }
+
+    public static class TimePickerFragment extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+            int minute = Calendar.getInstance().get(Calendar.MINUTE);
+
+
+            TimePickerDialog dpd = new TimePickerDialog(getActivity(), android.R.style.Theme_Holo_Light_Dialog,
+                    (TimePickerDialog.OnTimeSetListener) getActivity(), hour, minute, true);
+
+            dpd.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+            return dpd;
+        }
     }
 
 }
